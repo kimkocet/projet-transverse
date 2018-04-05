b08d1418a0594dbe8d18fb1a655a1f77a45e0f5f
elsach <37287960+elsach@users.noreply.github.com>
2018-04-05 10:15:37 +0200

    jeu terminé
    - jouer() pour jouer une partie avec 2 joueurs
    - conditions d'arrêt du jeu (jeuTermine())

--- src/jeu/Plateau.java
+++ src/jeu/Plateau.java
@@ -1,5 +1,7 @@
 package jeu;
 
+import java.util.Scanner;
+
 public class Plateau {
 
 	private static final int SIZE = 4;
@@ -46,5 +48,217 @@ public class Plateau {
 			s+="]\n";
 		}
 		return s;
+	}
+	
+	public void jouer(Joueur j1, Joueur j2) {
+		Scanner sc = new Scanner(System.in);
+		int l;
+		int c;
+		
+		boolean coup;
+		do {
+			do {
+				do {
+					System.out.println("entrer la ligne suivie de la colonne du coup à jouer (joueur 1) : ");
+					l = sc.nextInt();
+					c = sc.nextInt();
+				}while(l < 0 || l > 3 || c < 0 || c > 3);
+				coup = j1.jouerCoup(l, c, this);
+			} while(!coup);
+			System.out.println(this);
+			if(!jeuTermine(j1, j2)) {
+				do {
+					do {
+						System.out.println("entrer la ligne suivie de la colonne du coup à jouer (joueur 2) : ");
+						l = sc.nextInt();
+						c = sc.nextInt();
+					} while(l < 0 || l > 3 || c < 0 || c > 3);
+					coup = j2.jouerCoup(l, c, this);
+				} while(!coup);
+				System.out.println(this);
+				}
+				else {
+					break;
+				}
+		} while(!jeuTermine(j1, j2));
+		sc.close();
+	}
+	
+	public boolean jeuTermine(Joueur j1, Joueur j2) {
+		// tableau rempli
+		boolean tableauRempli = true;
+		for(int i = 0; i < SIZE; i++) {
+			for(int j = 0; j < SIZE; j++) {
+				if(plateau[i][j] == 0) tableauRempli = false;
+			}
+		}
+		if(tableauRempli) return true;
+		// 1 joueur a gagné (3 cases alignées)
+		// 1 ligne avec 3 cases à 1 ou 2
+		int compteurJ1 = 0;
+		int compteurJ2 = 0;
+		for(int i = 0; i < SIZE; i++) {
+			for(int col = 0; col < 2; col++) {
+				for(int j = 0; j < 3; j++) {
+					if(plateau[i][col+j] == j1.getNumero()) {
+						compteurJ1++;
+					}
+					else if(plateau[i][col+j] == j2.getNumero()) {
+						compteurJ2++;
+					}
+				}
+				if(compteurJ1 == 3) {
+					System.out.println("Le joueur 1 a gagné !");
+					return true;
+				}
+				if(compteurJ2 == 3) {
+					System.out.println("Le joueur 2 a gagné !");
+					return true;
+				}
+				compteurJ1 = 0;
+				compteurJ2 = 0;
+			}
+		}
+		// 1 colonne avec 3 cases à 1 ou 2
+		// parcourt de chaque colonne
+		for(int j = 0; j < SIZE; j++) {
+			// 2 possibilités: début à la première ou deuxième ligne
+			for(int ligne = 0; ligne < 2; ligne++) {
+				// 3 lignes successives
+				for(int i = 0; i < 3; i++) {
+					if(plateau[i+ligne][j] == j1.getNumero()) {
+						compteurJ1++;
+					}
+					else if(plateau[i+ligne][j] == j2.getNumero()) {
+						compteurJ2++;
+					}
+				}
+				if(compteurJ1 == 3) {
+					System.out.println("Le joueur 1 a gagné !");
+					return true;
+				}
+				if(compteurJ2 == 3) {
+					System.out.println("Le joueur 2 a gagné !");
+					return true;
+				}
+				compteurJ1 = 0;
+				compteurJ2 = 0;
+			}
+		}
+		// 1 diagonale (haut gauche à bas droite)
+		// 2 possibilitées, début à la case [0][0] ou à la case [1][1]
+		for(int deb = 0; deb < 2; deb++) {
+			for(int i = 0; i < 3; i++) {
+				if(plateau[deb+i][deb+i] == j1.getNumero()) {
+					compteurJ1++;
+				}
+				if(plateau[deb+i][deb+i] == j2.getNumero()) {
+					compteurJ2++;
+				}
+			}
+			if(compteurJ1 == 3) {
+				System.out.println("Le joueur 1 a gagné !");
+				return true;
+			}
+			if(compteurJ2 == 3) {
+				System.out.println("Le joueur 2 a gagné !");
+				return true;
+			}
+			compteurJ1 = 0;
+			compteurJ2 = 0;
+		}
+		// 1 diagonale inverse (haut droite à bas gauche)
+		for(int deb = 0; deb < 2; deb++) {
+			for(int i = 0; i < 3; i++) {
+				if(plateau[deb+i][SIZE-(deb+i)-1] == j1.getNumero()) {
+					compteurJ1++;
+				}
+				if(plateau[deb+i][SIZE-(deb+i)-1] == j2.getNumero()) {
+					compteurJ2++;
+				}
+			}
+			if(compteurJ1 == 3) {
+				System.out.println("Le joueur 1 a gagné !");
+				return true;
+			}
+			if(compteurJ2 == 3) {
+				System.out.println("Le joueur 2 a gagné !");
+				return true;
+			}
+			compteurJ1 = 0;
+			compteurJ2 = 0;
+		}
+		// petites diagonales
+		for(int i = 1; i < 4; i++) {
+			if(plateau[i][i-1] == j1.getNumero()) {
+				compteurJ1++;
+			}
+			if(plateau[i][i-1] == j2.getNumero()) {
+				compteurJ2++;
+			}
+		}
+		if(compteurJ1 == 3) {
+			System.out.println("Le joueur 1 a gagné !");
+			return true;
+		}
+		if(compteurJ2 == 3) {
+			System.out.println("Le joueur 2 a gagné !");
+			return true;
+		}
+		compteurJ1 = 0;
+		compteurJ2 = 0;
+		for(int i = 0; i < 3; i++) {
+			if(plateau[i][i+1] == j1.getNumero()) {
+				compteurJ1++;
+			}
+			if(plateau[i][i+1] == j2.getNumero()) {
+				compteurJ2++;
+			}
+		}
+		if(compteurJ1 == 3) {
+			System.out.println("Le joueur 1 a gagné !");
+			return true;
+		}
+		if(compteurJ2 == 3) {
+			System.out.println("Le joueur 2 a gagné !");
+			return true;
+		}
+		compteurJ1 = 0;
+		compteurJ2 = 0;
+		for(int i = 1; i < 4; i++) {
+			if(plateau[i][SIZE-i] == j1.getNumero()) {
+				compteurJ1++;
+			}
+			if(plateau[i][SIZE-i] == j2.getNumero()) {
+				compteurJ2++;
+			}
+		}
+		if(compteurJ1 == 3) {
+			System.out.println("Le joueur 1 a gagné !");
+			return true;
+		}
+		if(compteurJ2 == 3) {
+			System.out.println("Le joueur 2 a gagné !");
+			return true;
+		}
+		compteurJ1 = 0;
+		compteurJ2 = 0;
+		for(int i = 0; i < 3; i++) {
+			if(plateau[i][SIZE-2-i] == j1.getNumero()) {
+				compteurJ1++;
+			}
+			if(plateau[i][SIZE-2-i] == j2.getNumero()) {
+				compteurJ2++;
+			}
+		}
+		if(compteurJ1 == 3) {
+			System.out.println("Le joueur 1 a gagné !");
+			return true;
+		}
+		if(compteurJ2 == 3) {
+			System.out.println("Le joueur 2 a gagné !");
+			return true;
+		}
+		return false;	
 	}
 }
--- src/package1/Main.java
+++ src/package1/Main.java
@@ -1,7 +1,5 @@
 package package1;
 
-import java.util.Scanner;
-
 import jeu.Joueur;
 import jeu.Plateau;
 
@@ -17,19 +15,8 @@ public class Main {
 		Joueur j1 = new Joueur(1);
 		Joueur j2 = new Joueur(2);
 		
-		Scanner sc = new Scanner(System.in);
-		int l;
-		int c;
-		do {
-			System.out.println("entrer la ligne suivie de la colonne du coup à jouer : ");
-			l = sc.nextInt();
-			c = sc.nextInt();
-		}while(l < 0 || l > 3 || c < 0 || c > 3);
-		
-		j1.jouerCoup(l, c, p);
-		System.out.println(p);
-		
-		sc.close();
+		p.jouer(j1, j2);
+	
 	}
 
 }
